08146239bd2b55d572d544b2c11d0c9c0c456135
AV306 <brandonjoash@gmail.com>
2024-10-29 18:19:20 +0800

    g-translate: add escape-to-unfocus

--- extension/scripts/google-translate.js
+++ extension/scripts/google-translate.js
@@ -1,20 +1,26 @@
 let mainTextInput = document.getElementsByTagName( "textarea" )[0];
+let clearButton = document.querySelector( "button.GA2I6e" );
 
 document.addEventListener( "keydown", event =>
 {
-  if ( document.activeElement !== mainTextInput )
-  {
+    if ( event.repeat ) return;
     switch ( event.code )
     {
-      case "Slash":
-        mainTextInput.focus();
-        event.preventDefault();
-        break;
-      case "Backspace":
-        mainTextInput.value = "";
-        mainTextInput.focus();
-        event.preventDefault();
-        break;
+        case "Slash":
+            if ( document.activeElement == mainTextInput ) return;
+            mainTextInput.focus();
+            event.preventDefault();
+            break;
+        case "Backspace":
+            if ( document.activeElement == mainTextInput ) return;
+            //mainTextInput.value = "";
+            clearButton.click();
+            mainTextInput.focus();
+            event.preventDefault();
+            break;
+        case "Escape":
+            console.log( "iasdfh" );
+            clearButton.focus();
+            event.preventDefault();
     }
-  }
 } );
